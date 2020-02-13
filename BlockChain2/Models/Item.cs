540c8fcd4daa15ef2313e241c2f9c59631f48cbe
Spencer Jensen <swj96@byu.edu>
2020-02-12 20:38:58 -0700

    test

--- BlockChain2/Models/Item.cs
+++ BlockChain2/Models/Item.cs
@@ -7,5 +7,9 @@ namespace BlockChain2.Models
 {
     public class Item
     {
+        public string itemID { get; set; }
+        public string itemName { get; set; }
+        public string serialNo { get; set; }
+        public int quantityOfItem { get; set; }
     }
 }
--- BlockChain2/Models/User.cs
+++ BlockChain2/Models/User.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
 
@@ -7,6 +9,7 @@ namespace BlockChain2.Models
 {
     public class User
     {
+        
         public string userId { get; set; }
     }
 }
