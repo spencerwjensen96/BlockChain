a81c5ba2654b4f7ffe216b3dfb0db98f106c13c7
Spencer Jensen <swj96@byu.edu>
2020-02-19 19:52:03 -0700

    Logout Login fun

--- BlockChain2/Controllers/HomeController.cs
+++ BlockChain2/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Security;
 
 namespace BlockChain2.Controllers
 {
@@ -25,6 +26,13 @@ namespace BlockChain2.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+
+        [HttpGet]
+        public ActionResult Logout()
+        {
+            FormsAuthentication.SignOut();
+            return Redirect("/Home");
         }
     }
 }
